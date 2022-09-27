55613ec08c90eebc244908efed224d0712135697
Munkh-Altai Chuluunbaatar <munkh-altai@Munkh-Altais-MacBook-Pro-2.local>
2022-09-27 11:26:13 +0800

    squire logo added

--- src/config/defaultSettings.ts
+++ src/config/defaultSettings.ts
@@ -8,18 +8,18 @@
  * fixSiderbar - boolean
  * contentWidth - Content Area Layout: Fluid | Fixed
  */
-
+console.log(import.meta.env.LAMBDA_PRIMARY_COLOR)
+console.log(import.meta.env.LAMBDA_PRIMARY_COLOR)
+console.log(import.meta.env.LAMBDA_PRIMARY_COLOR)
 export default {
   navTheme: 'light', // theme for nav menu
-  primaryColor: '#3471f6',
+  primaryColor: import.meta.env.LAMBDA_PRIMARY_COLOR,
   layout: 'sidemenu', // nav menu position: `sidemenu` or `topmenu`
   contentWidth: 'Fluid', // layout of content: `Fluid` or `Fixed`, only works when layout is topmenu
   fixedHeader: false, // sticky header
   fixSiderbar: false, // sticky siderbar
   colorWeak: false,
-  title: 'Ant Design Pro',
-  // pwa: false,
-  // iconfontUrl: '',
+  title: 'Lambda',
   storage: {
     namespace: ''
   },
