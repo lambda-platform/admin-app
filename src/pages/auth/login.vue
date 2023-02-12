6afce2b6565d73d4c984b075f417ed6c26be7b14
munkh-altai <munkaltai@gmail.com>
2023-02-12 15:38:05 +0900

    OAuth2 support

--- src/pages/auth/login.vue
+++ src/pages/auth/login.vue
@@ -1,7 +1,6 @@
 <template>
   <component :is="renderTheme()" :onSuccess="onSuccess" :selectedLang="selectedLang"
              :lambda="lambda"
-
   ></component>
 </template>
 <script>
@@ -9,11 +8,21 @@ definePageMeta({
   layout: 'agent',
 })
 import axios from 'axios'
-import { ACCESS_TOKEN, PERMISSIONS, USER_INFO, LAMBDA_CONFIG, MENU, KRUDS, MENU_LIST, MICROSERVICE_SETTINGS } from '~/store/mutation-types'
+import {
+  ACCESS_TOKEN,
+  PERMISSIONS,
+  USER_INFO,
+  LAMBDA_CONFIG,
+  MENU,
+  KRUDS,
+  MENU_LIST,
+  MICROSERVICE_SETTINGS
+} from '~/store/mutation-types'
 import ls from '~/utils/Storage'
 import { createList } from '~/utils/menu'
 import { setToken } from '~/plugins/core/axios'
 import { defineAsyncComponent } from 'vue'
+
 export default {
   props: ['selectedLang'],
   name: 'login',
@@ -30,64 +39,63 @@ export default {
       if (data.status) {
         this.isSuccess = true
 
-        ls.set(USER_INFO, {
-          ...data.data,
-          jwt: undefined
-        })
-        window.init = {
-          user:data.data,
-          firebase_config: this.lambda.notify.firebaseConfig,
-          microserviceSettings:[],
-
-        }
-        ls.set(ACCESS_TOKEN, data.token, 7 * 24 * 60 * 60 * 1000)
-        setToken(data.token)
-        if (data.data.role === 1) {
-          window.location.replace(data.path)
+        if (data.oauth) {
+          window.location.replace('/oauth2/authorize')
         } else {
-
-          axios.get('/get-permissions').then((res) => {
-
-            if (res.data.status) {
-
-              ls.set(PERMISSIONS, res.data.permission.permissions)
-              ls.set(MENU, res.data.permission.menu)
-              ls.set(KRUDS, res.data.permission.kruds)
-              let menuList = createList(res.data.permission.menu, null, res.data.permission.kruds)
-              ls.set(MENU_LIST, menuList)
-
-              if(res.data.permission.microserviceSettings){
-                ls.set(MICROSERVICE_SETTINGS, res.data.permission.microserviceSettings)
-
-                window.init["microserviceSettings"] = res.data.permission.microserviceSettings
-
-              }
-
-
-
-              let path = data.path.replaceAll('#', '')
-              if (path.includes('/p/')) {
-                let paths = path.split('/')
-
-                let menu_id = paths[paths.length - 1]
-
-
-                // this.$router.replace('/admin/p/' + menu_id)
-                this.$router.replace('/admin')
-              } else {
-                // this.$router.replace(path)
-                this.$router.replace('/admin')
-              }
-
-
-            }
+          ls.set(USER_INFO, {
+            ...data.data,
+            jwt: undefined
           })
+          window.init = {
+            user: data.data,
+            firebase_config: this.lambda.notify.firebaseConfig,
+            microserviceSettings: [],
 
+          }
+          ls.set(ACCESS_TOKEN, data.token, 7 * 24 * 60 * 60 * 1000)
+          setToken(data.token)
+          if (data.data.role === 1) {
+            window.location.replace(data.path)
+          } else {
+
+            axios.get('/get-permissions').then((res) => {
+
+              if (res.data.status) {
+
+                ls.set(PERMISSIONS, res.data.permission.permissions)
+                ls.set(MENU, res.data.permission.menu)
+                ls.set(KRUDS, res.data.permission.kruds)
+                let menuList = createList(res.data.permission.menu, null, res.data.permission.kruds)
+                ls.set(MENU_LIST, menuList)
+
+                if (res.data.permission.microserviceSettings) {
+                  ls.set(MICROSERVICE_SETTINGS, res.data.permission.microserviceSettings)
+
+                  window.init['microserviceSettings'] = res.data.permission.microserviceSettings
+
+                }
+
+                let path = data.path.replaceAll('#', '')
+                if (path.includes('/p/')) {
+                  let paths = path.split('/')
+
+                  let menu_id = paths[paths.length - 1]
+
+                  // this.$router.replace('/admin/p/' + menu_id)
+                  this.$router.replace('/admin')
+                } else {
+                  // this.$router.replace(path)
+                  this.$router.replace('/admin')
+                }
+
+              }
+            })
+
+          }
         }
-
       }
     },
-    renderTheme(){
+    renderTheme () {
       return defineAsyncComponent(() => import(`../../../node_modules/@lambda-platform/lambda-vue/src/modules/agent/views/theme/${this.lambda.theme}/auth/login.vue`))
     }
 
