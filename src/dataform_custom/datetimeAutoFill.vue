6a1e452873a0df6b5c0be016cbba8986ccdd31b5
munkh-altai <munkaltai@gmail.com>
2023-09-27 13:25:08 +0800

    lambda version UP

--- src/dataform_custom/datetimeAutoFill.vue
+++ src/dataform_custom/datetimeAutoFill.vue
@@ -13,14 +13,14 @@
 <script>
 import mixin from '@lambda-platform/lambda-vue/src/modules/dataform/elements/_mixin'
 import axios from 'axios'
-import dayjs from 'dayjs';
+
 export default {
   mixins: [mixin],
   mounted () {
         if(this.itemValue === null){
             axios.get("/lambda/krud/today").then(({data})=>{
-                const dateFormat = 'YYYY-MM-DD';
-                this.model.form[this.model.component] = dayjs(data.today, dateFormat);
+
+                this.model.form[this.model.component] = data.today
             });
         }
     }
