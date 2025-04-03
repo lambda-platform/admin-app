66754f813c2399972561452b239e92e1f27d1861
Bilguun <4bilguun@gmail.com>
2025-04-03 09:41:03 +0800

    notif bug fix

--- src/app.vue
+++ src/app.vue
@@ -26,7 +26,6 @@
 import {ref, reactive, onBeforeMount} from 'vue';
 import {useStore} from 'vuex';
 import {useI18n} from 'vue-i18n';
-
 import mn_MN from 'ant-design-vue/lib/locale/mn_MN';
 import {
   LAMBDA_CONFIG,
@@ -42,9 +41,8 @@ import {setupI18n} from '@lambda-platform/lambda-vue/src/locale';
 import {primaryColor, darkMode} from '~/store/useSiteSettings';
 import {theme} from 'ant-design-vue';
 import {setUserPermissions} from '~/authencation/auth';
-const {darkAlgorithm, compactAlgorithm} = theme;
 
-// Vuex store and i18n
+const {darkAlgorithm, compactAlgorithm} = theme;
 const store = useStore();
 const {t, locale} = useI18n();
 
@@ -56,18 +54,14 @@ const mnLocale = {
 }
 
 const loading = ref(true);
-
 const algorithm = [darkAlgorithm, compactAlgorithm]
 
-
-// Methods
 const redirectToLogin = () => {
   clearUserInfo();
   loading.value = false;
 };
 
 const initialize = async () => {
-
   try {
     const res = await getLambdaConfig();
     ls.set(LAMBDA_CONFIG, res);
@@ -81,29 +75,22 @@ const initialize = async () => {
       window.microservice_dev = true;
     }
 
-    window.int = {
+    window.init = {
       firebase_config:res.notify.firebaseConfig
     }
 
-
     if (ls.get(USER_INFO)) {
-
-
       await setUserPermissions(store, res.notify.firebaseConfig, ls.get(USER_INFO));
-
       loading.value = false;
-
     } else {
       redirectToLogin();
     }
   } catch (error) {
-
     console.error(error);
     redirectToLogin();
   }
 };
 
-// Lifecycle hooks
 onBeforeMount(() => {
   window.withTimezone = true;
   window.onresize = setDeviceType;
