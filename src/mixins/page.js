3683274e09584457715990fa7c8d72236a5064db
munkh-altai <munkaltai@gmail.com>
2025-11-12 17:09:55 +0800

    page mixin fixed

--- src/mixins/page.js
+++ src/mixins/page.js
@@ -8,44 +8,44 @@ import SubTopMenu from '~/components/Menu/SubTopMenu.vue'
 
 export default {
   components: {
-    SubTopMenu
+    SubTopMenu,
   },
   computed: {
     menuMode() {
-      let menuModeSaved = localStorage.getItem('menuMode')
+      let menuModeSaved = localStorage.getItem("menuMode");
       if (menuModeSaved) {
-        return menuModeSaved
+        return menuModeSaved;
       } else {
-        return undefined
+        return undefined;
       }
     },
     isMobile() {
-      return isMobile
+      return isMobile;
     },
     path() {
-      return this.$route.fullPath
-    }
+      return this.$route.fullPath;
+    },
   },
   data() {
-    const kruds = ls.get(KRUDS)
-    const permissions = ls.get(PERMISSIONS)
-    const menu = ls.get(MENU)
-    const menu_list = ls.get(MENU_LIST)
-    const LambdaConfig = ls.get(LAMBDA_CONFIG)
+    const kruds = ls.get(KRUDS);
+    const permissions = ls.get(PERMISSIONS);
+    const menu = ls.get(MENU);
+    const menu_list = ls.get(MENU_LIST);
+    const LambdaConfig = ls.get(LAMBDA_CONFIG);
     return {
       options: {
-        height: '1000px'
+        height: "1000px",
       },
-      pageType: '',
+      pageType: "",
 
       property: {
         base_url: base_url,
         withCrudLog: LambdaConfig.withCrudLog,
         withoutHeader: false,
         page_id: null,
-        template: 'drawer',
-        mode: 'refresh',
-        title: '',
+        template: "drawer",
+        mode: "refresh",
+        title: "",
         projects_id: null,
         grid: null,
         form: null,
@@ -61,55 +61,54 @@ export default {
           d: false,
         },
       },
-      iframeUrl: '',
-      iframeTitle: '',
+      iframeUrl: "",
+      iframeTitle: "",
       submenu: [],
       showSub: false,
       cruds: kruds,
       menu: menu,
       menu_list: menu_list,
       permissions: permissions.permissions,
-      pageTitle: '',
-      subMenuId: '0',
+      pageTitle: "",
+      subMenuId: "0",
       showNestedMenu: false,
       subTopMenus: [],
       base_url,
-    }
+    };
   },
   methods: {
-
     getShowAbleChild(children) {
-      let showIndex = children.findIndex(child => this.can(child))
+      let showIndex = children.findIndex((child) => this.can(child));
 
       if (showIndex >= 0) {
-        return children[showIndex]
+        return children[showIndex];
       } else {
-        return null
+        return null;
       }
     },
 
     can(menu) {
       if (this.permissions[menu.id]) {
         if (this.permissions[menu.id].show) {
-          return true
+          return true;
         } else {
-          return false
+          return false;
         }
       } else {
-        return false
+        return false;
       }
     },
 
     getTitle(item) {
-      if (item.link_to == 'crud') {
-        let crudIndex = this.cruds.findIndex(crud => crud.id == item.url)
+      if (item.link_to == "crud") {
+        let crudIndex = this.cruds.findIndex((crud) => crud.id == item.url);
         if (crudIndex >= 0) {
-          return this.cruds[crudIndex].title
+          return this.cruds[crudIndex].title;
         } else {
-          return ''
+          return "";
         }
       } else {
-        return item.title
+        return item.title;
       }
     },
 
@@ -118,87 +117,107 @@ export default {
     // },
 
     getPage() {
+      this.menu_list = ls.get(MENU_LIST);
+      this.menu = ls.get(MENU);
+      const permissions = ls.get(PERMISSIONS);
+      if (permissions.permissions) {
+        this.permissions = permissions.permissions;
+      }
 
       if (this.menu_list) {
-
-        let page_index = this.menu_list.findIndex(m => m.id === this.$route.params.menu_id);
+        let page_index = this.menu_list.findIndex(
+          (m) => m.id === this.$route.params.menu_id
+        );
 
         if (page_index === -1) {
-          page_index = this.menu_list.findIndex(m => m.url === this.$route.path);
+          page_index = this.menu_list.findIndex(
+            (m) => m.url === this.$route.path
+          );
         }
 
         if (page_index >= 0) {
+          const page = this.menu_list[page_index];
 
-          const page = this.menu_list[page_index]
-
-          this.pageType = page.link_to
+          this.pageType = page.link_to;
           switch (this.pageType) {
-            case 'crud':
-              let crudIndex = this.cruds.findIndex(crud => crud.id == page.url)
+            case "crud":
+              let crudIndex = this.cruds.findIndex(
+                (crud) => crud.id == page.url
+              );
 
               if (crudIndex >= 0) {
-                this.$route.meta.title = this.cruds[crudIndex].title
+                this.$route.meta.title = this.cruds[crudIndex].title;
                 // this.property. = 'canvas'
 
-                this.property.parent = page.parent
-                this.property.title = this.cruds[crudIndex].title
+                this.property.parent = page.parent;
+                this.property.title = this.cruds[crudIndex].title;
                 // this.property.withoutHeader = this.withoutHeader;
-                this.property.projects_id = this.cruds[crudIndex].projects_id
-                this.property.grid = this.cruds[crudIndex].grid
-                this.property.form = this.cruds[crudIndex].form
-                this.property.edit_id = this.cruds[crudIndex].edit_id
-                this.property.template = this.cruds[crudIndex].template
+                this.property.projects_id = this.cruds[crudIndex].projects_id;
+                this.property.grid = this.cruds[crudIndex].grid;
+                this.property.form = this.cruds[crudIndex].form;
+                this.property.edit_id = this.cruds[crudIndex].edit_id;
+                this.property.template = this.cruds[crudIndex].template;
                 if (this.cruds[crudIndex].actions) {
-                  this.property.actions = this.cruds[crudIndex].actions
+                  this.property.actions = this.cruds[crudIndex].actions;
                 }
-                this.property.main_tab_title = this.cruds[crudIndex].main_tab_title
-                this.property.form_width = this.cruds[crudIndex].form_width ? this.cruds[crudIndex].form_width : null
-                this.property.view_url = this.cruds[crudIndex].view_url
-                this.property.permissions.c = this.permissions[page.id].c
-                this.property.permissions.r = this.permissions[page.id].r
-                this.property.permissions.u = this.permissions[page.id].u
-                this.property.permissions.d = this.permissions[page.id].d
-                this.property.permissions.gridDeleteConditionJS = this.permissions[page.id].gridDeleteConditionJS
-                this.property.permissions.gridEditConditionJS = this.permissions[page.id].gridEditConditionJS
-                let user_condition = {}
+                this.property.main_tab_title =
+                  this.cruds[crudIndex].main_tab_title;
+                this.property.form_width = this.cruds[crudIndex].form_width
+                  ? this.cruds[crudIndex].form_width
+                  : null;
+                this.property.view_url = this.cruds[crudIndex].view_url;
+                this.property.permissions.c = this.permissions[page.id].c;
+                this.property.permissions.r = this.permissions[page.id].r;
+                this.property.permissions.u = this.permissions[page.id].u;
+                this.property.permissions.d = this.permissions[page.id].d;
+                this.property.permissions.gridDeleteConditionJS =
+                  this.permissions[page.id].gridDeleteConditionJS;
+                this.property.permissions.gridEditConditionJS =
+                  this.permissions[page.id].gridEditConditionJS;
+                let user_condition = {};
 
                 if (this.permissions[page.id].formCondition) {
-                  user_condition.formCondition = this.permissions[page.id].formCondition
+                  user_condition.formCondition =
+                    this.permissions[page.id].formCondition;
                 }
                 if (this.permissions[page.id].gridCondition) {
-                  user_condition.gridCondition = this.permissions[page.id].gridCondition
+                  user_condition.gridCondition =
+                    this.permissions[page.id].gridCondition;
                 }
 
                 if (user_condition) {
-                  this.property.user_condition = user_condition
+                  this.property.user_condition = user_condition;
                 }
-                this.property.page_id = page.id
+                this.property.page_id = page.id;
               }
-              break
-            case 'link':
-
-              window.location = this.menu_list[page_index]['url']
-              break
-            case 'router-link':
-
-              this.$router.push(this.menu_list[page_index]['url'])
-              break
-            case 'iframe':
-              this.iframeUrl = page.url
-              this.property.title = page.title
-              break
+              break;
+            case "link":
+              window.location = this.menu_list[page_index]["url"];
+              break;
+            case "router-link":
+              this.$router.push(this.menu_list[page_index]["url"]);
+              break;
+            case "iframe":
+              this.iframeUrl = page.url;
+              this.property.title = page.title;
+              break;
             default:
-              break
+              break;
           }
 
           // Check if the page has a parent
           if (page.parent?.length >= 1) {
             // Find the index of the parent with link_to = "noActionSubTop"
-            const pIndex = page.parent.findIndex(p => p.link_to === "noActionSubTop");
+            const pIndex = page.parent.findIndex(
+              (p) => p.link_to === "noActionSubTop"
+            );
 
             // If the parent exists, find its children and set them as the subTopMenus
             if (pIndex >= 0) {
-              const {children} = findMenuItemById(this.menu, page.parent[pIndex].id);
+              const { children } = findMenuItemById(
+                this.menu,
+                page.parent[pIndex].id
+              );
 
               if (children?.length) {
                 this.subTopMenus = children;
@@ -207,12 +226,17 @@ export default {
           }
         }
       }
-
-    }
+    },
   },
   beforeMount() {
-
     this.getPage();
   },
-
-}
+  watch: {
+    "$route.fullPath": {
+      immediate: false,
+      handler() {
+        this.getPage();
+      },
+    },
+  },
+};
